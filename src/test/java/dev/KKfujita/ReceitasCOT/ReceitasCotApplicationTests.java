0b94c06c613fe155ab4ae5a4711eae80dac7a772
Kauã Kyuzo Fujita <271039221+KauaFuijtakkyz@users.noreply.github.com>
2026-05-13 18:41:38 -0300

    test: adicionar estrutura inicial para testes unitários

---
+++ src/test/java/dev/KKfujita/ReceitasCOT/ReceitasCotApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.KKfujita.ReceitasCOT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceitasCotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
